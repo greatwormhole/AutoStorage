2c0bf8f7f9be06c379493b219ece1706f6944ea8
nikita@greatwormhole <nikita.chistyakov2002@gmail.com>
2023-07-06 16:47:16 +0300

    THDList

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import DeliveryView, base, LoginView
+from .views import DeliveryView, base, LoginView, THDList
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('barcode', DeliveryView.as_view(), name='barcode'),
     path('API/login', LoginView.as_view(), name='login'),
+    path('API/THD_list', THDList.as_view(), name='THD_list'),
     path('', base.as_view(), name="base"),
 ]
 
