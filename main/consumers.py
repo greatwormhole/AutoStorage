c9e1fde82870f117b56d1d6cf9af8e7323fde55a
nikita@greatwormhole <nikita.chistyakov2002@gmail.com>
2023-08-02 10:34:20 +0300

    Started websockets

--- main/consumers.py
+++ main/consumers.py
@@ -120,3 +120,26 @@ class THDWS(WebsocketConsumer):
                 "username": event["username"],
                 "message": event["message"],
             }))
+
+class StorageVisualizing(WebsocketConsumer):
+    
+    def connect(self):
+        
+        self.room_name = 'TEST'
+        
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name,
+            self.channel_name
+        )
+        
+        self.accept()
+    
+    def disconnect(self, code):
+        
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_name,
+            self.channel_name
+        )
+    
+    def receive(self, text_data=None, bytes_data=None):
+        return super().receive(text_data, bytes_data)
