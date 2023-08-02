ba03d09538c12e0391d547f9115d64899a3adad3
ilyxxxa <ilya.yuldashev@gmail.com>
2023-08-02 10:35:06 +0300

    штеукафсу

--- static/storage/js/visualization.js
+++ static/storage/js/visualization.js
@@ -8,7 +8,8 @@ function zeroCount(arr){
         count++
         }
     }
-    return 1+count/arr.length
+
+    return 1-count/arr.length
 }
 
 function zeroCountNum(arr){
@@ -28,7 +29,8 @@ function buildStorages(storageList){
     for (let i=0; i<storageList.length; i++){
         cells = getCells(storageList[i])
         var storageCell = [],
-            rowNumber
+            rowNumber,
+            storageName=storageList[i]
         for (let j = 0; j<cells.length; j++){
             rowNumber = cells[j].fields.y_cell_coord
             if (storageCell[rowNumber] == null){
@@ -46,14 +48,14 @@ function buildStorages(storageList){
             }
         }
         cellPleasesArray.push(storageCell)
-
+        cellInformation = cellPleasesArray
         var storageDiv = $('#' + storageList[i]),
             screenWidth = parseInt($('#view-space').css('width')),
             height = storageCell.map((elem) => elem[0][0][1]).reduce((partialSum, a) => partialSum + a, 0)*Math.max.apply(Math,bouncer(storageCell.map((elem) => Math.max.apply(Math, elem.map((elem) => elem.length))))),
             scaleY = screenWidth * 1 / height
 
         storageCell.forEach(function(row,idx){
-            var htmlRow = '<div class = "storageRow" id = "'+idx+'_st_row">',
+            var htmlRow = '<div class = "storageRow" id = "'+idx+'_st_row_'+storageName+'">',
                     rowIdx = idx
                 htmlRow += '</div>'
                 $(storageDiv).append(htmlRow)
@@ -81,37 +83,66 @@ function buildStorages(storageList){
             }
 
             lengthList = scaleXList
-            scaleXList = scaleXList.map((length, idx) => (screenWidth-countLayer[idx]*8)/(length*zeroCount(planLayer[idx])))
+            scaleXList = scaleXList.map((length, idx) => (screenWidth-planLayer[idx].length*8)/(length/(zeroCount(planLayer[idx]))))
+            console.log(scaleXList)
             row.forEach(function(column, idx){
                 var columnIdx = idx
                 column.forEach(function(layer,idx){
-                    if ($('#'+idx+'_'+rowIdx+'_st_layer').length==0){
-                    var htmlLayer = '<div id="'+idx+'_'+rowIdx+'_st_layer" class="storageLayer">'
+                    if ($('#'+idx+'_'+rowIdx+'_st_layer_'+storageName).length==0){
+                    var htmlLayer = '<div id="'+idx+'_'+rowIdx+'_st_layer_'+storageName+'" class="storageLayer">'
                     htmlLayer += '</div>'
-                    $('#'+rowIdx+'_st_row').append(htmlLayer)
+                    $('#'+rowIdx+'_st_row_'+storageName).append(htmlLayer)
                     }
                     var i = columnIdx
-                    while (planLayer[idx][i+1] == 0){
 
-                        html = '<div class = "space" style = "width:'+parseFloat(lengthList[idx]*(zeroCount(planLayer[idx])-1)*scaleXList[idx]/(zeroCountNum(planLayer[idx]))-2)+'px;">'
-                        html += '</div>'
-                        $('#'+idx+'_'+rowIdx+'_st_layer').append(html)
-                        i++
-                    }
-                    console.log(layer[0])
-                    console.log(layer[1])
+
+                    if (scaleXList[idx]>maxScale){
+                        var html = '<div class = "storageCell" style = "width:'+layer[0]*maxScale+'px; height:'+layer[1]*maxScale+'px">'
+                    html += '</div>'
+
+                    $('#'+idx+'_'+rowIdx+'_st_layer_'+storageName).append(html)
+                    } else{
                     var html = '<div class = "storageCell" style = "width:'+layer[0]*scaleXList[idx]+'px; height:'+layer[1]*scaleXList[idx]+'px">'
                     html += '</div>'
 
-                    $('#'+idx+'_'+rowIdx+'_st_layer').append(html)
+                    $('#'+idx+'_'+rowIdx+'_st_layer_'+storageName).append(html)
+                    }
+                    while (planLayer[idx][i+1] == 0){
+                        console.log(lengthList[idx]/(zeroCount(planLayer[idx]))-lengthList[idx])
+                        var html = '<div class = "space" style = "width:'+parseFloat((lengthList[idx]/(zeroCount(planLayer[idx]))-lengthList[idx])*scaleXList[idx]/(zeroCountNum(planLayer[idx]))+6)+'px;max-width:'+parseFloat(parseFloat($('.storageCell').css('width'))+6)+'px">'
+                        html += '</div>'
+
+
+                        $('#'+idx+'_'+rowIdx+'_st_layer_'+storageName).append(html)
+                        i++
+                    }
                 })
             })
         })
-
+       storageMargin(storageCell,storageName,false)
 
 
     }
 
+}
+function storageMargin(storageCell,storageName, isUnMargin){
+    storageCell.forEach(function(row,idx){
+            var rowIdx = idx
+            row.forEach(function(column,idx){
+                column.forEach(function(layer,idx){
+                    if(idx!=0){
+                        var prevRowHeight = parseFloat($('#'+parseInt(idx-1)+'_'+rowIdx+'_st_layer_'+storageName).css('height'))
+                        if (typeof prevRowHeight != 'undefined'){
+                            if(!isUnMargin){
+                                $('#'+idx+'_'+rowIdx+'_st_layer_'+storageName).css({'margin-top':'-'+prevRowHeight*0.8+'px','margin-left': idx*marginLeft+'px'})
+                            } else{
+                                $('#'+idx+'_'+rowIdx+'_st_layer_'+storageName).css({'margin-top':'0px','margin-left': '0px'})
+                            }
+                        }
+                    }
+                })
+            })
+        })
 }
 
 function getCells(id){
@@ -154,4 +185,17 @@ function canvasDraw(){
     ctx.lineTo(175,0)
     ctx.stroke()
   }
+}
+
+function OCStorage(id){
+    var storage=id.replace('_OC',''),
+        storageNameList = JSON.parse(storageList.replaceAll('&#x27;','"'))
+    if ($('#'+id).css('transform') == 'none' || $('#'+id).css('transform') == 'matrix(1, 0, 0, 1, 0, 0)'){
+        $('#'+storage).children().css({'margin-top':'0px','margin-bottom':'0px'})
+        storageMargin(cellInformation[storageNameList.indexOf(storage)],storage,false)
+        $('#'+id).css({'transform':'rotate(180deg)'})
+        return
+    }
+    storageMargin(cellInformation[storageNameList.indexOf(storage)],storage,true)
+    $('#'+id).css({'transform':'rotate(0deg)'})
 }
